4dbd62e0b425c008ec4b8b68386957f754644328
Bobby Wang <wbo4958@gmail.com>
2022-07-15 22:45:49 +0800

    support cuda ipc

---
+++ sql-plugin/src/main/java/com/nvidia/spark/rapids/CudfColumn.java
@@ -0,0 +1,151 @@
+/*
+ Copyright (c) 2021 by Contributors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package com.nvidia.spark.rapids;
+
+import java.util.Base64;
+
+import com.nvidia.spark.rapids.jni.CudaIPC;
+
+import ai.rapids.cudf.BaseDeviceMemoryBuffer;
+import ai.rapids.cudf.BufferType;
+import ai.rapids.cudf.ColumnVector;
+import ai.rapids.cudf.DType;
+
+/**
+ * This class is composing of base data with Apache Arrow format from Cudf ColumnVector.
+ * It will be used to generate the cuda array interface.
+ */
+public class CudfColumn {
+
+  private final long dataPtr; //  gpu data buffer address
+  private final long shape;   // row count
+  private final long validPtr; // gpu valid buffer address
+  private final int typeSize; // type size in bytes
+  private final String typeStr; // follow array interface spec
+  private final long nullCount; // null count
+
+  private String arrayInterface = null; // the cuda array interface
+
+  public static CudfColumn from(ColumnVector cv) {
+    BaseDeviceMemoryBuffer dataBuffer = cv.getDeviceBufferFor(BufferType.DATA);
+    BaseDeviceMemoryBuffer validBuffer = cv.getDeviceBufferFor(BufferType.VALIDITY);
+    long validPtr = 0;
+    if (validBuffer != null) {
+      validPtr = validBuffer.getAddress();
+    }
+    DType dType = cv.getType();
+    String typeStr = "";
+    if (dType == DType.FLOAT32 || dType == DType.FLOAT64 ||
+        dType == DType.TIMESTAMP_DAYS || dType == DType.TIMESTAMP_MICROSECONDS ||
+        dType == DType.TIMESTAMP_MILLISECONDS || dType == DType.TIMESTAMP_NANOSECONDS ||
+        dType == DType.TIMESTAMP_SECONDS) {
+      typeStr = "<f" + dType.getSizeInBytes();
+    } else if (dType == DType.BOOL8 || dType == DType.INT8 || dType == DType.INT16 ||
+        dType == DType.INT32 || dType == DType.INT64) {
+      typeStr = "<i" + dType.getSizeInBytes();
+    } else {
+      // Unsupported type.
+      throw new IllegalArgumentException("Unsupported data type: " + dType);
+    }
+
+    return new CudfColumn(dataBuffer.getAddress(), cv.getRowCount(), validPtr,
+      dType.getSizeInBytes(), typeStr, cv.getNullCount());
+  }
+
+  private CudfColumn(long dataPtr, long shape, long validPtr, int typeSize, String typeStr,
+                     long nullCount) {
+    this.dataPtr = dataPtr;
+    this.shape = shape;
+    this.validPtr = validPtr;
+    this.typeSize = typeSize;
+    this.typeStr = typeStr;
+    this.nullCount = nullCount;
+  }
+
+  public String getArrayInterfaceJson() {
+    // There is no race-condition
+    if (arrayInterface == null) {
+      arrayInterface = buildColumnCudaIpcInfo(this);
+    }
+    return arrayInterface;
+  }
+
+  public long getDataPtr() {
+    return dataPtr;
+  }
+
+  public long getShape() {
+    return shape;
+  }
+
+  public long getValidPtr() {
+    return validPtr;
+  }
+
+  public int getTypeSize() {
+    return typeSize;
+  }
+
+  public String getTypeStr() {
+    return typeStr;
+  }
+
+  public long getNullCount() {
+    return nullCount;
+  }
+
+
+  /** build the whole column information including data and valid info */
+  private String buildColumnCudaIpcInfo(CudfColumn column) {
+    if (column.getDataPtr() == 0) {
+      throw new IllegalArgumentException("Empty column data is NOT accepted!");
+    }
+    if (column.getTypeStr() == null || column.getTypeStr().isEmpty()) {
+      throw new IllegalArgumentException("Empty type string is NOT accepted!");
+    }
+
+    StringBuilder builder = new StringBuilder();
+    String colData = buildMetaObject(column.getDataPtr(), column.getShape(),
+        column.getTypeStr());
+    builder.append("{");
+    builder.append(colData);
+    if (column.getValidPtr() != 0 && column.getNullCount() != 0) {
+      String validString = buildMetaObject(column.getValidPtr(), column.getShape(), "<t1");
+      builder.append(",\"mask\":");
+      builder.append("{");
+      builder.append(validString);
+      builder.append("}");
+    }
+    builder.append("}");
+    return builder.toString();
+  }
+
+  /** build the base information of a column */
+  private String buildMetaObject(long ptr, long shape, final String typeStr) {
+
+    byte[] bytes = CudaIPC.getCudaIpcMemHandle(ptr);
+    String cudaIpcInfo = Base64.getEncoder().encodeToString(bytes);
+
+    StringBuilder builder = new StringBuilder();
+    builder.append("\"shape\":[" + shape + "],");
+    builder.append("\"data\":[" + cudaIpcInfo + "," + "false" + "],");
+    builder.append("\"typestr\":\"" + typeStr + "\",");
+    builder.append("\"version\":" + 1);
+    return builder.toString();
+  }
+
+}
---
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/python/GpuArrowCudaIcEvalPythonExec.scala
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.sql.rapids.execution.python
+
+import java.io.DataOutputStream
+import java.net.Socket
+
+import ai.rapids.cudf.{ArrowIPCWriterOptions, NvtxColor, NvtxRange, Table}
+import com.nvidia.spark.rapids.{CudfColumn, GpuColumnVector, GpuMetric, GpuSemaphore}
+import org.apache.arrow.vector.VectorSchemaRoot
+import org.apache.arrow.vector.ipc.ArrowStreamWriter
+
+import org.apache.spark.{SparkEnv, TaskContext}
+import org.apache.spark.api.python.{ChainedPythonFunctions, PythonRDD}
+import org.apache.spark.sql.catalyst.expressions.GenericInternalRow
+import org.apache.spark.sql.execution.arrow.ArrowWriter
+import org.apache.spark.sql.execution.python.PythonUDFRunner
+import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.types.{ArrayType, DataType, MapType, StringType, StructField, StructType}
+import org.apache.spark.sql.util.ArrowUtils
+import org.apache.spark.sql.vectorized.ColumnarBatch
+import org.apache.spark.util.Utils
+
+/**
+ * Similar to `PythonUDFRunner`, but exchange data with Python worker via Arrow stream.
+ */
+class GpuArrowCudaIcEvalPythonExec(
+    funcs: Seq[ChainedPythonFunctions],
+    evalType: Int,
+    argOffsets: Array[Array[Int]],
+    pythonInSchema: StructType,
+    timeZoneId: String,
+    conf: Map[String, String],
+    batchSize: Long,
+    semWait: GpuMetric,
+    onDataWriteFinished: () => Unit,
+    pythonOutSchema: StructType,
+    minReadTargetBatchSize: Int = 1)
+  extends GpuArrowPythonRunner(funcs, evalType, argOffsets, pythonInSchema,
+    timeZoneId, conf, batchSize, semWait, onDataWriteFinished,
+    pythonOutSchema, minReadTargetBatchSize) {
+
+  override val bufferSize: Int = SQLConf.get.pandasUDFBufferSize
+  require(
+    bufferSize >= 4,
+    "Pandas execution requires more than 4 bytes. Please set higher buffer. " +
+      s"Please change '${SQLConf.PANDAS_UDF_BUFFER_SIZE.key}'.")
+
+  protected override def newWriterThread(
+    env: SparkEnv,
+    worker: Socket,
+    inputIterator: Iterator[ColumnarBatch],
+    partitionIndex: Int,
+    context: TaskContext): WriterThread = {
+    new WriterThread(env, worker, inputIterator, partitionIndex, context) {
+
+      protected override def writeCommand(dataOut: DataOutputStream): Unit = {
+
+        // Write config for the worker as a number of key -> value pairs of strings
+        dataOut.writeInt(conf.size)
+        for ((k, v) <- conf) {
+          PythonRDD.writeUTF(k, dataOut)
+          PythonRDD.writeUTF(v, dataOut)
+        }
+
+        PythonUDFRunner.writeUDFs(dataOut, funcs, argOffsets)
+      }
+
+      private def getTableIpcInfo(table: Table): Array[Any] = {
+        val columnIpcInfo = new Array[Any](table.getNumberOfColumns)
+        for (i <- 0 until table.getNumberOfColumns) {
+          columnIpcInfo(i) = CudfColumn.from(table.getColumn(i)).getArrayInterfaceJson
+        }
+        columnIpcInfo
+      }
+
+      protected override def writeIteratorToStream(dataOut: DataOutputStream): Unit = {
+        // create schema with all string types
+        val structFields = pythonInSchema.fieldNames.map(name => {
+          StructField(name, StringType, nullable = true)
+        })
+        val fixSchema = StructType(structFields)
+        val arrowSchema = ArrowUtils.toArrowSchema(fixSchema, timeZoneId)
+        val allocator = ArrowUtils.rootAllocator.newChildAllocator(
+          s"stdout writer for $pythonExec", 0, Long.MaxValue)
+        val root = VectorSchemaRoot.create(arrowSchema, allocator)
+
+        Utils.tryWithSafeFinally {
+          val arrowWriter = ArrowWriter.create(root)
+          val writer = new ArrowStreamWriter(root, null, dataOut)
+          writer.start()
+
+          while (inputIterator.hasNext) {
+            val table = withResource(inputIterator.next()) { nextBatch =>
+              GpuColumnVector.from(nextBatch)
+            }
+            val columnIpcInfo: Array[Any] = getTableIpcInfo(table)
+            val genericInternalRow = new GenericInternalRow(columnIpcInfo)
+            arrowWriter.write(genericInternalRow)
+            arrowWriter.finish()
+            writer.writeBatch()
+            arrowWriter.reset()
+          }
+          // end writes footer to the output stream and doesn't clean any resources.
+          // It could throw exception if the output stream is closed, so it should be
+          // in the try block.
+          writer.end()
+        } {
+          // If we close root and allocator in TaskCompletionListener, there could be a race
+          // condition where the writer thread keeps writing to the VectorSchemaRoot while
+          // it's being closed by the TaskCompletion listener.
+          // Closing root and allocator here is cleaner because root and allocator is owned
+          // by the writer thread and is only visible to the writer thread.
+          //
+          // If the writer thread is interrupted by TaskCompletionListener, it should either
+          // (1) in the try block, in which case it will get an InterruptedException when
+          // performing io, and goes into the finally block or (2) in the finally block,
+          // in which case it will ignore the interruption and close the resources.
+          root.close()
+          allocator.close()
+        }
+      }
+
+      private def flattenNames(d: DataType, nullable: Boolean = true): Seq[(String, Boolean)] =
+        d match {
+          case s: StructType =>
+            s.flatMap(sf => Seq((sf.name, sf.nullable)) ++ flattenNames(sf.dataType, sf.nullable))
+          case m: MapType =>
+            flattenNames(m.keyType, nullable) ++ flattenNames(m.valueType, nullable)
+          case a: ArrayType => flattenNames(a.elementType, nullable)
+          case _ => Nil
+        }
+    }
+  }
+
+}
