cb873fc8f4a5931ac6f518e34722fae5a8bdea3c
Gary Shen <gashen@nvidia.com>
2025-05-14 16:34:52 +0800

    Enable test
    Signed-off-by: Gary Shen <gashen@nvidia.com>

--- tests/src/test/spark330/scala/org/apache/spark/sql/rapids/utils/RapidsTestSettings.scala
+++ tests/src/test/spark330/scala/org/apache/spark/sql/rapids/utils/RapidsTestSettings.scala
@@ -105,5 +105,6 @@ class RapidsTestSettings extends BackendTestSettings {
     .exclude("SPARK-22550: Elt should not generate codes beyond 64KB", WONT_FIX_ISSUE("https://github.com/NVIDIA/spark-rapids/issues/10775"))
     .exclude("SPARK-22603: FormatString should not generate codes beyond 64KB", WONT_FIX_ISSUE("https://github.com/NVIDIA/spark-rapids/issues/10775"))
   enableSuite[RapidsStringFunctionsSuite]
+  enableSuite[RapidsDateFunctionsSuite]
 }
 // scalastyle:on line.size.limit
